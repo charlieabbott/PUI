62c99f0ae72140df1f045e604c4924b80a74e185
Charlie Abbott <charles.b.abbott@gmail.com>
2018-10-01 11:45:51 -0400

    ya
    hoo

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -14,6 +14,7 @@ function addListItem() {
 function deleteListItem(item) {
 	    // remove li element (item) from ol element (item.parentNode)
  	    item.parentNode.removeChild(item);
+        console.log("test");
 }
 
 function completeListItem(item) {
@@ -22,19 +23,29 @@ function completeListItem(item) {
         // in css, this would be: "text-decoration: line-through"
     } else {
         item.parentNode.style.textDecoration = "none";
+        console.log("test")
         // in css, this would be: "text-decoration: none"
     }
 }
 
 $(document).ready(function(){
-    $("#add-item").click(function() { // bind handler for click event
+    /*$("#add-item").click(function() { // bind handler for click event
         var list = $("#grocery-list"); // get the ol list by id
         var itemInput = $("#new-list-item"); // get the new item input
       // append the input value within an li element
-        list.append("<li>" + itemInput.val() + "</li>"); 
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>"); 
     });
         $(".delete-item").click(function() {
         $(this).parent().remove();
+        console.log("test")
+    });*/
+        $(document).on("click", ".delete-item", function() {
+        $(this).parent().remove();
     });
 
+    $(document).on("click", "#add-item", function() {
+        var list = $("#grocery-list");
+        var itemInput = $("#new-list-item");
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+    });
 });
